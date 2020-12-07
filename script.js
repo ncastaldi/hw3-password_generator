8ec405d0c21b0525e3a65a33f9e09196fae0c83c
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-07 18:32:10 -0500

    added prompts for pw length and LCase letters

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword() {
 
-var setLCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var setUCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var setSpecial = ["!", "#", "$", "%", "&", "*", "+", "=", "?", "@", "^"];
-var setNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var pwdLength = prompt("Password Length?\nEnter a number between 1 and 128");
-
-for (i = 1; i <= pwdLength; i++) {
+  /* Start declarations */
+  var setLCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var setUCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var setSpecial = ["!", "#", "$", "%", "&", "*", "+", "=", "?", "@", "^"];
+  var setNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  maxSet = [];
+  password = [];
+  /* End declarations */
   
+  /* Ask user how long password should be */
+  var pwdLength = prompt("Password Length?\nEnter a number between 1 and 128");
+  pwdLength = Math.ceil(pwdLength); // Account for people entering in non-whole numbers.
+
+  var includeLCase = confirm("Would you like to include lowercase letters?");
+  if (includeLCase) {
+    for (i = 0; i < setLCase.length); i++{
+      maxSet[i] = setLCase[i];
+    }
   }
-}
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-}
+  for (i = 0; i <= maxSet; i++) {
+    password[i] = maxSet[Math.random(maxSet)];
+    }
+  
+  return password;
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
